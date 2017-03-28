6750047cef838522c351b2e82207bd3674d18a7f
julienDaoustCistec <jdaoust@cistec.ca>
2017-03-28 14:13:27 -0400

    Adding all files

---
+++ StackLayoutExos/StackLayoutExos2Page.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace StackLayoutExos
+{
+	public partial class StackLayoutExos2Page : ContentPage
+	{
+		public StackLayoutExos2Page()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ StackLayoutExos/StackLayoutExosPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StackLayoutExos
+{
+	public partial class StackLayoutExosPage : ContentPage
+	{
+		public StackLayoutExosPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
